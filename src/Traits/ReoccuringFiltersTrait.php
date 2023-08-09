397174e5fbf594756d73cdc81f29794e08302f52
= <=>
2023-08-09 11:37:47 +0200

    Added Label to dropdown filter

--- src/Traits/ReoccuringFiltersTrait.php
+++ src/Traits/ReoccuringFiltersTrait.php
@@ -7,13 +7,17 @@ use Illuminate\Support\Str;
 trait ReoccuringFiltersTrait
 {
 
-    protected function dropdown_filter($_name, $_options){
+    protected function dropdown_filter(
+        $_name,
+        $_options,
+        $_label = null,
+    ){
         // dropdown filter
         $name = $_name . '_id';
         $this->crud->addFilter([
             'name'  => $name,
             'type'  => 'dropdown',
-            'label' =>  ucwords(Str::replace('_', ' ', $_name)),
+            'label' =>  ($_label == null)? ucwords(Str::replace('_', ' ', $_name)) : $_label,
         ],
         $_options,
         function($value) use($name) {
